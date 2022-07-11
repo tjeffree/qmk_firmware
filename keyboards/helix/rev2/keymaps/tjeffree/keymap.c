40ab0fa336f1c44bd88172476ba7461ebb67759d
Tony <tjeffree@gmail.com>
2022-07-11 10:24:20 +0100

    Keymap updates

--- keyboards/helix/rev2/keymaps/tjeffree/keymap.c
+++ keyboards/helix/rev2/keymaps/tjeffree/keymap.c
@@ -47,8 +47,8 @@ enum custom_keycodes {
 #define LSLSH LSFT_T(KC_NUBS)
 #define LBRKT LSFT(KC_9)
 #define RBRKT LSFT(KC_0)
-#define RCTLT RCTL_T(KC_MINS)
-#define RGUIT RGUI_T(KC_EQL)
+#define RGUIT RGUI_T(KC_SPC)
+#define RCTLT RCTL_T(KC_SPC)
 
 const uint16_t PROGMEM combo_minus[] = {KC_9, KC_0, COMBO_END};
 const uint16_t PROGMEM combo_plus[] = {KC_0, KC_BSPC, COMBO_END};
@@ -92,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty                                                                            | - |  | = |
  * ,-----------------------------------------.             ,-----------------------------------------.
- * | Esc  |  1   |  2   |  3   |  4   |  5   |             |  6   |  7   |  8   |  9   |  0   | BkSp |
+ * | Esc  |  1   |  2   |  3   [  4   ]  5   |             |  6   {  7   }  8   |  9   |  0   | BkSp |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------| Home
- * | Tab  |  Q   [  W   ]  E   |  R   |  T   |             |  Y   |  U   |  I   {  O   }  P   | Del  |
+ * | Tab  |  Q   |  W   |  E   |  R   |  T   |             |  Y   |  U   |  I   |  O   |  P   | Del  |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------| PgUp
  * | Caps |  A   |  S   |  D   |  F   |  G   |             |  H   |  J   |  K   |  L   |  ;   |  '   |
  * |------+------+------+------+------+------|------+------|------+------+------+------+------+------| PgDn
@@ -104,11 +104,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-------------------------------------------------------------------------------------------------'
  */
   [_QWERTY] = LAYOUT(
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      LSLSH,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MRSFT ,
-      KC_LCTL, OS_SWP,  KC_LGUI, KC_LALT, CD,      KC_SPC,  RCTLT,   RGUIT,   KC_SPC,  FN,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      LSLSH,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MRSFT ,
+      KC_LCTL, OS_SWP,  KC_LGUI, KC_LALT, CD,      KC_SPC, KC_SPC , RGUIT  , RCTLT,   FN,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
       ),
 
 /* Func
