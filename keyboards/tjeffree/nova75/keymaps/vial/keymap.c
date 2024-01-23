920d012e36b2557e7a91cf071874b440a320da6b
Tony <tjeffree@gmail.com>
2024-01-23 18:09:36 +0000

    Update keymap

--- keyboards/tjeffree/nova75/keymaps/vial/keymap.c
+++ keyboards/tjeffree/nova75/keymaps/vial/keymap.c
@@ -3,25 +3,12 @@
 enum layers {
   _QWERTY = 0,
   _FUNCTION,
-  _LAST,
-  _OS
-};
-
-enum custom_keycodes {
-  PHP_OP = SAFE_RANGE,
-  PHP_CL,
-  PHP_SH, // Short php tag open
-  OS_SWAP,
-  OS_SWAP_PC,
-  OS_SWAP_MAC,
-  ARROW_LEFT,
-  ARROW_RGHT,
+  _LAST
 };
 
 #define FN MO(_FUNCTION)
 #define LST MO(_LAST)
 #define MRSFT MT(MOD_RSFT, KC_ENT)
-#define OS_SWP MO(_OS)
 #define LSLSH LSFT_T(KC_NUBS)
 #define RCTLT RCTL_T(KC_SPC)
 
@@ -45,14 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_PGDN, KC_INS , KC_END , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
       KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_PSCR, KC_GRV , KC_PAUS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       LSLSH,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_UP  , KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MRSFT ,
-      KC_LCTL,   OS_SWP,  KC_LGUI, KC_LALT, FN,      KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, RCTLT,   FN,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      KC_LCTL,   KC_LGUI, KC_LGUI, KC_LALT, FN,      KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, RCTLT,   FN,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 
 	[_FUNCTION] = LAYOUT_ortho_5x15(
 		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6  , KC_TRNS, KC_TRNS, KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 ,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_NUBS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, PHP_OP , PHP_CL , PHP_SH , KC_TRNS,
+		KC_TRNS, KC_NUBS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LST,     KC_TRNS, KC_TRNS, LST,     KC_TRNS, KC_TRNS, LST,     KC_HOME, KC_PGDN, KC_PGUP, KC_END
         ),
 
@@ -62,84 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS, KC_SLEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, LST,     KC_TRNS, KC_TRNS, LST,     KC_TRNS, KC_TRNS, LST,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-        ),
-
-	[_OS] = LAYOUT_ortho_5x15(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS   , KC_TRNS, KC_TRNS   , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS   , KC_TRNS, KC_TRNS   , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, OS_SWAP_PC, KC_TRNS, OS_SWAP_PC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS   , KC_TRNS, KC_TRNS   , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, OS_SWAP, KC_TRNS   , KC_TRNS, KC_TRNS   , OS_SWAP, KC_TRNS, OS_SWAP_MAC, KC_TRNS, KC_TRNS, OS_SWAP_PC
-        ),
+        )
 
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-
-        // PHP macros
-        case PHP_OP:
-            if (record->event.pressed) {
-                SEND_STRING("<?php");
-            }
-            break;
-        case PHP_CL:
-            if (record->event.pressed) {
-                SEND_STRING("?>");
-            }
-            break;
-        case PHP_SH:
-            if (record->event.pressed) {
-                SEND_STRING("<?=");
-            }
-            break;
-
-        // OS swapping
-        case OS_SWAP:
-            if (record->event.pressed) {
-                keymap_config.swap_lctl_lgui = !keymap_config.swap_lctl_lgui;
-            }
-            break;
-
-        case OS_SWAP_MAC:
-            if (record->event.pressed) {
-                tap_code16(LGUI(KC_LEFT));
-                keymap_config.swap_lctl_lgui = true;
-            }
-            break;
-
-        case OS_SWAP_PC:
-            if (record->event.pressed) {
-                tap_code16(LGUI(KC_RGHT));
-                keymap_config.swap_lctl_lgui = false;
-            }
-            break;
-
-        // Code stuff
-        case ARROW_LEFT:
-            if (record->event.pressed) {
-                if (get_mods() & MOD_MASK_SHIFT) {  // Is shift held?
-                    del_mods(MOD_MASK_SHIFT);  // Temporarily delete shift.
-                    SEND_STRING("<=");
-                } else {
-                    SEND_STRING("<-");
-                }
-            }
-            break;
-        case ARROW_RGHT:
-            if (record->event.pressed) {
-                if (get_mods() & MOD_MASK_SHIFT) {  // Is shift held?
-                    del_mods(MOD_MASK_SHIFT);  // Temporarily delete shift.
-                    SEND_STRING("=>");
-                } else {
-                    SEND_STRING("->");
-                }
-            }
-            break;
-
-    }
-  return true;
-}
 
 void matrix_init_user(void) {
     // Set to PC/Windows by default
